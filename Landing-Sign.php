5a5fd76cfa8318c8cd1e79c5ea7274192dc70479
geers15 <146058853+geers15@users.noreply.github.com>
2024-02-23 12:25:21 -0800

    Update Landing-Sign.php
    Fixed the error. Would not go to home page after correct log in. Also fixed where when wrong information was entered it wouldn't take you anywhere but lines of code. Also implemented a pop up window for when the user enters wrong information.

--- Landing-Sign.php
+++ Landing-Sign.php
@@ -1,51 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
-<head><title>Landing-Sign.php</title></head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AlertAcademy-Login</title>
+        <link rel="stylesheet" href="AlertAcademy.css">
+    </head>
+    <body>
+        <header>
+            <h2 class="title">AlertAcademy</h2>
+            <nav class="addLinks">
+                <a href="#">Contact Us</a>
+                <a href="#">FAQ</a>
+                <a href="#">Donate</a>
+                <button class="btnForLogin">Login</button>
+            </nav>
+        </header>
+        
+        <div class="wrapper">
+            <span class="icon-close"><ion-icon name="close-outline"></ion-icon></span>
+            
+            <div class="box login">
+                <h2>Login</h2>
+                <form action="http://localhost:8888/AlertAcademyWebsite-main/Landing-Sign.php" method="post">
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
+                        <input type="text" name="usr" required>
+                        <label>Username</label>
+                    </div>
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                        <input type="password" name="pass" required>
+                        <label>Password</label>
+                    </div>
+                    <div class="remem-forgot">
+                        <label><input type="checkbox">
+                        Remember Me</label>
+                        <a href="#">Forgot Password?</a>
+                    </div>
+                    <button type="submit" class="btn">Login</button>
+                    <div class="login-register">
+                        <p>Don't have an account? <a href="#" class="register">Register</a></p>
+                    </div>
+                </form>
+            </div>
 
-<?php
-/* Change the value of $password if you have set a password on the root userid
-*  Change NULL to port number to use DBMS other than the default using port 3306
-*/
-$user = 'root';
-$password = 'Access1998!'; //To be completed if you have set a password to root
-$database = 'AlertAcademy'; //To be completed to connect to a database. The database must exist.
-$port = NULL; //Default must be NULL to use default port
-$mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
+            <div class="box register">
+                <h2>Registration</h2>
+                <form action="#">
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
+                        <input type="text" required>
+                        <label>Username</label>
+                    </div>
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="person"></ion-icon></span>
+                        <input type="text" required>
+                        <label>First</label>
+                    </div>
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="person"></ion-icon></span>
+                        <input type="text" required>
+                        <label>Last</label>
+                    </div>
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                        <input type="email" required>
+                        <label>Email</label>
+                    </div>
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                        <input type="password" required>
+                        <label>Password</label>
+                    </div>
+                    <div class="remem-forgot">
+                        <label><input type="checkbox">I agree to the terms & conditions</label>
+                        
+                    </div>
+                    <button type="submit" class="btn">Register</button>
+                    <div class="login-register">
+                        <p>Already have an account? <a href="#" class="login-link">Login</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
 
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
-}
-else{
-	$usr = $_POST["usr"];
-	$pass = $_POST["pass"];
-	
-	$q = "SELECT UsrNm, Password FROM Person WHERE UsrNm = '$usr' && Password = '$pass'";
-	
-	//print("$q");
-	
-	$query = $mysqli->query("$q");
-	
-	$count = $query->num_rows;
-	
-	//print ("$count");
-	
-	if ($count == 1) {
-		include('AlertAcademyHomeScreenCode.html');
-	}
-	else {						// only looks at base html file, not CSS of JavaScript
-		//header( 'Location: C:\Users\steve\Documents\School\CSCI 450 Software Engineering\Project\Program Files\HTML - AlertAcademy.html' );
-		//exit();
-		include('AlertAcademy.js');
-		include('AlertAcademy.css');
-		include('AlertAcademy.html'); // only includes this, not css or js
-	}
-}		// if match, go to page, else, send message to user
-		// move code into HTML file because of message?
 
-$mysqli->close();
-?>
-
-</body>
+        <script src= "AlertAcademy.js"></script>
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </body>
 </html>
