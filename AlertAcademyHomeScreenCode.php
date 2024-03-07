d55834282082b27c38e4fa754aa51dae86d27978
Steven Andrew Gonzalez <91234937+Steve-Gonz@users.noreply.github.com>
2024-03-07 14:38:53 -0800

    Add files via upload

---
+++ AlertAcademyHomeScreenCode.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Alert Academy </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+	body {
+  		margin: 0;
+  		font-family: "Lato", sans-serif;
+		}
+
+	.sidebar {
+  		margin: 0;
+  		padding: 0;
+ 		width: 200px;
+  		background-color: #f1f1f1;
+  		position: fixed;
+  		height: 100%;
+  		overflow: auto;
+		}
+
+	.sidebar a {
+  		display: block;
+  		color: black;
+  		padding: 16px;
+  		text-decoration: none;
+		}
+ 
+	.sidebar a.active {
+  		background-color: #04AA6D;
+  		color: white;
+		}
+
+	.sidebar a:hover:not(.active) {
+  		background-color: #555;
+  		color: white;
+		}
+
+	div.content {
+  		margin-left: 200px;
+  		padding: 1px 16px;
+  		height: 1000px;
+		}
+
+	@media screen and (max-width: 700px) {
+  		.sidebar {
+    			width: 100%;
+    			height: auto;
+    			position: relative;
+  		}
+  	.sidebar a {float: left;}
+  	div.content {margin-left: 0;}
+	}
+
+	@media screen and (max-width: 400px) {
+  		.sidebar a {
+    			text-align: center;
+    			float: none;
+  			}
+	}
+</style>
+
+<script> 
+let User = sessionStorage.getItem('Current_User');	<!-- tells page which user to get information for -->
+alert(User);
+</script>
+
+<script type=“text/javascript”>
+   
+</script>
+
+		
+	</head>
+	<body>
+	<h1 align="center">Alert Academy</h1>
+		<!-- Navigation -->
+		<div class="sidebar">
+			<a class="active" href="profile">Profile</a> <!--Add in the command for the profile section, Angelu-->
+			<a href="home">Home</a> <!-- Try to add another code to return to the Home Screen even though it already in the Home Screen, Angelu-->
+			<a href="course">+Course</a> <!--This is the course link or coding would be located in, Angelu-->
+			<a href="assignment">+Assignment</a> <!-- This is where the adding assignment is going to be located so I am just waiting on the codes for these to work when it comes to the html codes, Angelu-->
+			<a href="setting">Settings</a> <!--This is where the settings link is going to be connected considering that in the template that I made show what the setting will look like, Angelu-->
+			<a href="help">Help</a> <!-- This is the help button; however, I do not know if we should add the help button, Angelu -->
+		</div>
+		<!--This is where the home screen tab will be located, I just need to fixed the layout of it., Angelu-->
+		<!--In addition the tab actually works I just need the connection of the other code to be connected to the home tab, the Profile icon, the +Course tab, and the +Assignment tab, Angelu-->
+	</body>
+</html>
