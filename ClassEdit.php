23478bbf20d39858fec6c1ca39f767a2ae54d1eb
Steven Andrew Gonzalez <91234937+Steve-Gonz@users.noreply.github.com>
2024-04-02 12:35:35 -0700

    Update ClassEdit.php

--- ClassEdit.php
+++ ClassEdit.php
@@ -25,11 +25,6 @@
   		padding: 16px;
   		text-decoration: none;
 		}
- 
-	.sidebar a.active {
-  		background-color: #04AA6D;
-  		color: white;
-		}
 
 	.sidebar a:hover:not(.active) {
   		background-color: #555;
@@ -109,7 +104,7 @@ let User = sessionStorage.getItem('Current_User');	<!-- tells page which user to
 		<div class="sidebar">
 			<a href="AlertAcademyHomeScreenCode.php">Home</a> <!--Add in the command for the profile section, Angelu-->
 			<a href="profile">Profile</a> <!-- Try to add another code to return to the Home Screen even though it already in the Home Screen, Angelu-->
-			<a class="active" href="course">+Course</a> <!--This is the course link or coding would be located in, Angelu-->
+			<a href="Course.php">+Course</a> <!--This is the course link or coding would be located in, Angelu-->
 			<a href="assignment">+Assignment</a> <!-- This is where the adding assignment is going to be located so I am just waiting on the codes for these to work when it comes to the html codes, Angelu-->
 			<a href="setting">Settings</a> <!--This is where the settings link is going to be connected considering that in the template that I made show what the setting will look like, Angelu-->
 			<a href="help">Help</a> <!-- This is the help button; however, I do not know if we should add the help button, Angelu -->
@@ -129,9 +124,6 @@ let User = sessionStorage.getItem('Current_User');	<!-- tells page which user to
 							. $Con->connect_error);
 				}
 				else{
-					session_start();
-					$_SESSION['thisuser'] = $_POST["ClassEditUser"];	// tells PHP code which user is in use
-					
 					$Class = $_POST["ClassEdit"];
 					
 					$q = "SELECT* FROM Cls where ClsID = '$Class'";
@@ -140,43 +132,45 @@ let User = sessionStorage.getItem('Current_User');	<!-- tells page which user to
 					$count = $Query->num_rows;
 					
 					if ($count == 1){
-						echo "<div style='text-align:center'>";
-						echo "<table border='1' class = \"ThisClass\">";
-						
 						while($row = $Query->fetch_assoc()) {
 							$Cname = $row["ClsName"];
 							$Col = $row["Color"];
 							$Prof = $row["ProfName"];
 							$Sch = $row["School"];
-							
-							echo "<form action='http://localhost/AlertAcademyHomeScreenCode.html'>";
-							echo "<label for='name'>Class name:</label> <input type='text' id='name' name='name' value='$Cname'><br>";
-							echo "<select Class = 'Drop' name=colors> <option class='gray' value= '0'>Gray</option> <option class='blue' value= '1'>Blue</option> <option class='red' value= '2'>Red</option> <option class='green' value= '3'>Green</option> <option class='yellow' value= '4'>Yellow</option> <option class='orange' value= '5'>Orange</option> <option class='purple' value= '6'>Purple</option> <option class='brown' value= '7'>Brown</option> </select>";
-							echo "<br> <label for='professor'>Professor name:</label> <input type='text' id='professor' name='professor' value='$Prof'> <br>";
-							echo "<label for='school'>School:</label> <input type='text' id='school' name='school' value='$Sch'><br>";
-							// submit button on click
-							echo "</form>";
+							$ID = $row["ClsID"];
+							$Per = $row["Person_ID"];
+							// get all data
 						}
 						
-						echo "</table>";
+						echo "<div style='text-align:center'>";
+						
+						echo "<form id='edit' action='http://localhost/ClassEdit2.php' method='post'>";
+						echo "<label for='name'>Class name:</label> <input type='text' id='name' name='name' value='$Cname'><br>";
+						echo "<select Class = 'Drop' name=colors> <option class='gray' value= '0'>Gray</option> <option class='blue' value= '1'>Blue</option> <option class='red' value= '2'>Red</option> <option class='green' value= '3'>Green</option> <option class='yellow' value= '4'>Yellow</option> <option class='orange' value= '5'>Orange</option> <option class='purple' value= '6'>Purple</option> <option class='brown' value= '7'>Brown</option> </select>";
+						echo "<br> <label for='professor'>Professor name:</label> <input type='text' id='professor' name='professor' value='$Prof'> <br>";
+						echo "<label for='school'>School:</label> <input type='text' id='school' name='school' value='$Sch'><br>";
+						echo "<input type='hidden' name='ID' value='$ID'>";
+						echo "<input type='hidden' name='P' value='$Per'>";
+						echo "<button type='submit' class='btn'>Save</button>";
+						echo "</form>";
+						
 						echo "</div>";
 					}
 					else{
-						echo "location.replace('AlertAcademyHomeScreenCode.html')";
+						include('AlertAcademyHomeScreenCode.php');
 					}
 				}
-
 				$Con->close();
-				?>
+			?>
 		</div>
-
+		
 		<script>
 			function logoutFunction() {
-			  <?php session_destroy(); ?>
 			  
-			  	location.replace("AlertAcademy.html")
+			  location.replace("http://localhost/AlertAcademy.html")
 			}
 		</script>
+		
 		<!--This is where the home screen tab will be located, I just need to fixed the layout of it., Angelu-->
 		<!--In addition the tab actually works I just need the connection of the other code to be connected to the home tab, the Profile icon, the +Course tab, and the +Assignment tab, Angelu-->
 	</body>
